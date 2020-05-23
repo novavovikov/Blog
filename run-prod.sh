f0fbb429c7ba7eff3833753c8bb2b2af45e6226e
Vladimir Novikov <novavovikov@gmail.com>
2020-05-23 20:28:06 +0300

    adds FLASK_ENV

--- run-prod.sh
+++ run-prod.sh
@@ -7,5 +7,5 @@ export GROUP_ID=$(id -g)
 export FLASK_ENV=production
 export COMPOSE_FILE=./docker-compose.yml
 
-docker-compose build
+docker-compose build --no-cache
 docker-compose up --force-recreate -d

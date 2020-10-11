db7f377dfa32bc11be2363aba2d680ec71a08c70
Vladimir Novikov <vladimir.novikov@genestack.com>
2020-10-12 01:59:14 +0300

    run proxy from docker-compose.prod.yml

--- run-prod.sh
+++ run-prod.sh
@@ -7,22 +7,5 @@ export GROUP_ID=$(id -g)
 export DOCKER_CLIENT_TIMEOUT=120
 export COMPOSE_HTTP_TIMEOUT=120
 
-#settings
-NETWORK="blog_net"
-PROXY_DOCKERFILE="$PWD/proxy/Dockerfile"
-PROXY_DOCKERFILE_TAG="proxy_nginx"
-
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
-
-#run proxy
-docker rm -f "$PROXY_DOCKERFILE_TAG" || true
-docker build --no-cache \
-  --force-rm \
-  -f "$PROXY_DOCKERFILE" \
-  -t "$PROXY_DOCKERFILE_TAG" .
-
-docker run --network "$NETWORK" \
-  --name "$PROXY_DOCKERFILE_TAG" \
-  -p 80:80 -p 443:443 \
-  -d "$PROXY_DOCKERFILE_TAG"
